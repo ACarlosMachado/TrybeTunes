60aa6b34a8758b17e60f8b387a187ca34b2b277b
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2022-02-11 01:25:31 -0300

    :heavy_check_mark: Feat: Requisito 2- cria uma formulario para identificaçao

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import { Redirect } from 'react-router';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      loading: false,
+      redirect: false,
+
+    };
+    this.getOnCLick = this.getOnCLick.bind(this);
+  }
+
+  async getOnCLick() {
+    const getName = document.querySelector('input').value;
+    console.log(getName);
+    this.setState({ loading: true });
+    await createUser({ name: getName });
+    this.setState({ loading: false, redirect: true });
+  }
+
   render() {
+    const {
+      loading,
+      name,
+      redirect,
+    } = this.state;
+    const valorMinAceitavel = 3;
     return (
       <div data-testid="page-login">
-        Login
+        <input
+          type="text"
+          data-testid="login-name-input"
+          placeholder="Digite seu nome"
+          onChange={ (e) => this.setState({ name: e.target.value }) }
+        />
+        <button
+          type="submit"
+          data-testid="login-submit-button"
+          id="btn-login"
+          disabled={ name.length < valorMinAceitavel }
+          onClick={ this.getOnCLick }
+        >
+          Entrar
+        </button>
+        { loading ? <Loading /> : Login }
+        { redirect ? <Redirect to="/search" /> : ''}
       </div>
     );
   }
