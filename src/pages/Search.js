27281c399aa57a65a31af9a88331bb3125674fc7
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2022-02-12 02:36:00 -0300

    :construction: feat: Requisito 6 - com erros no ESLint

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,36 +1,98 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import '../css/Search.css';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       nameArtist: '',
-
+      loading: false,
+      cardArtist: [],
+      phraseErro: '',
+      phraseNameArtist: false,
     };
+    this.searchArtist = this.searchArtist.bind(this);
+  }
+
+  async searchArtist() {
+    const getName = document.querySelector('input').value;
+    this.setState({ loading: true, nameArtist: getName, phraseErro: '' });
+    const result = await searchAlbumsAPI(getName);
+    const erroGetAlbum = 'Nenhum álbum foi encontrado';
+    this.setState({ loading: false,
+      cardArtist: result,
+      phraseErro: erroGetAlbum,
+      phraseNameArtist: true });
   }
 
   render() {
-    const { nameArtist } = this.state;
+    const {
+      loading,
+      nameArtist,
+      cardArtist,
+      phraseErro,
+      phraseNameArtist,
+    } = this.state;
     const valorMinAceitavel = 2;
+    const erro = phraseErro; // atribui a uma const para parar erro lint;
+
     return (
       <div data-testid="page-search">
         <Header />
-        <section>
-          <input
-            type="text"
-            data-testid="search-artist-input"
-            placeholder="Nome do artista"
-            onChange={ (e) => this.setState({ nameArtist: e.target.value }) }
-          />
-          <button
-            type="submit"
-            data-testid="search-artist-button"
-            disabled={ nameArtist.length < valorMinAceitavel }
-          >
-            Pesquisar
-          </button>
-        </section>
+        {
+          loading
+            ? <Loading />
+            : <section id="formArtist">
+              <input
+                type="text"
+                data-testid="search-artist-input"
+                placeholder="Nome do artista"
+                onChange={ (e) => this.setState({ nameArtist: e.target.value }) }
+              />
+              <button
+                type="submit"
+                data-testid="search-artist-button"
+                disabled={ nameArtist.length < valorMinAceitavel }
+                onClick={ this.searchArtist }
+              >
+                Pesquisar
+              </button>
+            </section>
+
+        }
+
+        {
+          phraseNameArtist
+            ? <p>{`Resultado de álbuns de: ${nameArtist} `}</p>
+            : ''
+        }
+
+        { cardArtist.length === 0
+          ? erro
+          : cardArtist.map((artist) => (
+            <div key={ artist.collectionId } className="card-artist">
+              <Link
+                to={ `/album/${artist.collectionId}` }
+                data-testid={ `link-to-album-${artist.collectionId}` }
+              >
+                <h4>
+                  { artist.artistName }
+                </h4>
+                <img
+                  src={ artist.artworkUrl100 }
+                  alt={ `foto de ${artist.collectionName}` }
+                />
+                <h5>
+                  { artist.collectionName }
+                </h5>
+              </Link>
+            </div>
+
+          ))}
       </div>
     );
   }
